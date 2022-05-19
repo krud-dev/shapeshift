c924faa5d181600a74d4d98f2857c527dfa9a2f4
Idan Elhalwani <idan@elhalwani.com>
2022-05-19 13:40:59 +0300

    docs(examples): add simple-example

---
+++ example/kotlin/simple-mapping/src/main/kotlin/dev/krud/shapeshift/examples/kotlin/Main.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright KRUD 2022
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package dev.krud.shapeshift.examples.kotlin
+
+import dev.krud.shapeshift.ShapeShiftBuilder
+
+/**
+ * This example shows how to use ShapeShift to map one object to another
+ */
+fun main() {
+    // Initialize ShapeShift with the default configuration
+    val shapeShift = ShapeShiftBuilder()
+        .build()
+    // Initialize a SimpleEntity
+    val simpleEntity = SimpleEntity(
+        name = "Test",
+        description = "Test description",
+        privateData = "Test private data"
+    )
+    // Map the SimpleEntity to a SimpleEntityDisplay using ShapeShift
+    val simpleEntityDisplay = shapeShift.map(simpleEntity, SimpleEntityDisplay::class.java)
+    println("SimpleEntityDisplay: $simpleEntityDisplay")
+}
