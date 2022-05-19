6929c0abbc1ef285774e9aec6f4b4d079b0162ce
Idan Elhalwani <idan@elhalwani.com>
2022-05-19 14:31:33 +0300

    docs(examples): add transformer-mapping example

---
+++ example/kotlin/transformer-mapping/src/main/kotlin/dev/krud/shapeshift/examples/kotlin/SimpleEntity.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright KRUD 2022
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package dev.krud.shapeshift.examples.kotlin
+
+import dev.krud.shapeshift.annotation.DefaultMappingTarget
+import dev.krud.shapeshift.annotation.MappedField
+import java.util.*
+
+@DefaultMappingTarget(SimpleEntityDisplay::class) // The default target class for the mapping
+data class SimpleEntity(
+    @MappedField
+    val creationDate: Date, // This field will be mapped to the creationDate field of the target class, and use the DateToLongTransformer implicitly
+
+    @MappedField(transformer = StringToCommaSeparatedStringListTransformer::class, mapTo = "stringList")
+    val commaDelimitedString: String, // This field will be mapped to the stringList field of the target class, and use the StringToCommaSeparatedStringListTransformer as it is explicitly specified
+)
