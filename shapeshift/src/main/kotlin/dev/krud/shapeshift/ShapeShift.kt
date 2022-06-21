0bfb73328c303774f50f57b5e0fca41a2d4e1fbb
Idan Elhalwani <idan@elhalwani.com>
2022-06-21 17:46:19 +0300

    Add programmatic mapping

--- shapeshift/src/main/kotlin/dev/krud/shapeshift/ShapeShift.kt
+++ shapeshift/src/main/kotlin/dev/krud/shapeshift/ShapeShift.kt
@@ -236,3 +236,4 @@ class ShapeShift constructor(
         }
     }
 }
+
--- shapeshift/src/main/kotlin/dev/krud/shapeshift/dsl/MappedFieldDsl.kt
+++ shapeshift/src/main/kotlin/dev/krud/shapeshift/dsl/MappedFieldDsl.kt
@@ -8,14 +8,7 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package dev.krud.shapeshift.dto
+package dev.krud.shapeshift.dsl
 
-import java.lang.reflect.Field
-
-data class ResolvedMappedField(val mapFromCoordinates: List<Field>, val mapToCoordinates: List<Field>, val transformerCoordinates: TransformerCoordinates = TransformerCoordinates.NONE) {
-    companion object {
-        fun ResolvedMappedField.withTransformer(transformerCoordinates: TransformerCoordinates): ResolvedMappedField {
-            return ResolvedMappedField(mapFromCoordinates, mapToCoordinates, transformerCoordinates)
-        }
-    }
-}
+@DslMarker
+annotation class MappedFieldDsl
