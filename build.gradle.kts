0ec19dee702d2a116c51c8e4c0d6c1bd10694e5e
Idan Elhalwani <idan@elhalwani.com>
2024-06-01 18:23:04 +0300

    chore: add GH workflows (#25)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,52 +1,60 @@
 import org.jetbrains.dokka.gradle.DokkaTask
+import java.util.*
 
 plugins {
     `java-library`
     `maven-publish`
     signing
     id("org.jetbrains.dokka") version "1.6.0"
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 
 if (hasProperty("release")) {
-    val releaseVersion = extra["shapeshift.version"].toString()
-    subprojects {
-        apply(plugin = "java-library")
-        apply(plugin = "signing")
-        apply(plugin = "maven-publish")
-        apply(plugin = "org.jetbrains.dokka")
+    val ossrhUsername = System.getenv("OSSRH_USERNAME")
+    val ossrhPassword = System.getenv("OSSRH_PASSWORD")
+    val releaseVersion = System.getenv("RELEASE_VERSION")
+    group = "dev.krud"
+    version = releaseVersion
+    nexusPublishing {
+        this@nexusPublishing.repositories {
+            sonatype {
+                username.set(ossrhUsername)
+                password.set(ossrhPassword)
+                nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+                snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            }
+        }
+    }
+}
+
+allprojects {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+    apply(plugin = "signing")
+
+    if (hasProperty("release")) {
+        val releaseVersion = System.getenv("RELEASE_VERSION")
+        val signingKeyBase64 = System.getenv("OSSRH_GPG_SECRET_KEY_BASE64")
+        val signingPassword = System.getenv("OSSRH_GPG_SECRET_KEY_PASSWORD")
         group = "dev.krud"
         version = releaseVersion
-        java.sourceCompatibility = JavaVersion.VERSION_1_8
         val isSnapshot = version.toString().endsWith("-SNAPSHOT")
-        val repoUri = if (isSnapshot) {
-            "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-        } else {
-            "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-        }
-
-        if (!isSnapshot) {
-            java {
-                withJavadocJar()
-                withSourcesJar()
-            }
+        java {
+            withJavadocJar()
+            withSourcesJar()
         }
 
         publishing {
             publications.create<MavenPublication>("maven") {
                 from(components["java"])
-                version = releaseVersion
-                repositories {
-                    maven {
-                        name = "OSSRH"
-                        url = uri(repoUri)
-                        credentials {
-                            username = System.getenv("OSSRH_USERNAME") ?: extra["ossrh.username"]?.toString()
-                            password = System.getenv("OSSRH_PASSWORD") ?: extra["ossrh.password"]?.toString()
-                        }
-                    }
-                }
+                artifactId = project.name
+
                 pom {
-                    name.set(this@subprojects.name)
+                    name.set(project.name)
                     description.set("A Kotlin library for intelligent object mapping.")
                     url.set("https://github.com/krud-dev/shapeshift/shapeshift")
                     licenses {
@@ -74,17 +82,22 @@ if (hasProperty("release")) {
             }
         }
 
-        if (!isSnapshot) {
-            val javadocTask = tasks.named<Javadoc>("javadoc").get()
+        val javadocTask = tasks.named<Javadoc>("javadoc").get()
 
-            tasks.withType<DokkaTask> {
-                javadocTask.dependsOn(this)
-                outputDirectory.set(javadocTask.destinationDir)
-            }
-
-            signing {
-                sign(publishing.publications["maven"])
-            }
+        tasks.withType<DokkaTask> {
+            javadocTask.dependsOn(this)
+            outputDirectory.set(javadocTask.destinationDir)
+        }
+        signing {
+            val signingKey = signingKeyBase64?.let { decodeBase64(it) }
+            useInMemoryPgpKeys(
+                signingKey, signingPassword
+            )
+            sign(publishing.publications["maven"])
         }
     }
+}
+
+fun decodeBase64(base64: String): String {
+    return String(Base64.getDecoder().decode(base64))
 }
