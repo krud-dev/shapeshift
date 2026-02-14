dda7c680b0f6da0ae3d89f2f9ac91223b2c7984f
Idan Elhalwani <idan.elh@gmail.com>
2026-02-14 19:59:54 +0200

    fix: fix OSSRH urls

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,8 @@ if (hasProperty("release")) {
             sonatype {
                 username.set(ossrhUsername)
                 password.set(ossrhPassword)
-                nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-                snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+                nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+                snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
             }
         }
     }
